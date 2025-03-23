524dc633c72ce7615701a9b7df7434b53c3874c3
Vladimir Támara Patiño <vtamar@pasosdeJesus.org>
2025-03-23 05:07:35 -0500

    Webhooks allows to analyze FonBnk Webhook tests

--- packages/react-app/app/api/webhooks/route.ts
+++ packages/react-app/app/api/webhooks/route.ts
@@ -1,28 +1,23 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import { NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 
-export async function POST(request) {
-//req: NextApiRequest, res: NextApiResponse, resolvedUrl) {
-  console.log("request=", request)
-  //console.log("req=", req, "res=", res, "resolvedUrl=", resolvedUrl)
-  return NextResponse.json({ good: 'All good' }, { status: 200 })
-  return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 })
-  if (req.method === 'POST') {
-    const event = req.body.event; // Extract event data
-    // Handle the event
-    console.log('Received event:', event);
-    // Perform necessary actions based on the event
-    //res.status(200).json({ message: 'Webhook received successfully' });
-    res.setHeader("Content-Type", "application/json");
-    res.write('{\"market\": \"x\", \"prices\": 10}');
-    res.end();
-    return {
-      props: {},
-    }
-  } else {
-    console.log('Problem');
-    if (typeof res != "undefined" && typeof res.status != "undefined") {
-      res.status(405).json({ error: 'Method Not Allowed' });
-    }
+export async function POST(request: NextRequest) {
+  //console.log("request=", request)
+  //console.log("request.cookies=", request.cookies)
+  console.log("request.headers=", request.headers)
+
+  try {
+    //const text = await request.text()
+    const requestJson = await request.json()
+    console.log("request.json()=", requestJson)
+    // Process the webhook payload
+
+    return NextResponse.json({ good: 'All good' }, { status: 200 })
+  } catch (error) {
+    return NextResponse.json({ error: 'Webhook error' }, { status: 400})
   }
+
+  //const searchParams = request.nextUrl.searchParams
+  //return NextResponse.json({ good: 'All good' }, { status: 200 })
+  // console.log('Problem');
+  // return NextResponse.json({ error: 'Method Not Allowed' }, { status: 405})
 }
