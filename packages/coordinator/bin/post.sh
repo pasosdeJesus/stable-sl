f948ae8551bea1a859451311dda165887278d7f3
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2025-05-24 22:22:46 -0400

    typo

--- packages/coordinator/bin/post.sh
+++ packages/coordinator/bin/post.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-phone=075934442
+phone=075333222
 amount=25
 curl -H 'Content-Type: application/json' \
-  -d "{\"sender\":\"${phone}\",\"msg\":\"Transaction Id 09ABC12 Transfer Succesful from ${phone} transaction amount SLE${amount} net credit amount SLE${amount} your new balance is SLE100\"}" \
+  -d "{\"sender\":\"OrangeMoney\",\"msg\":\"Transaction Id 09ABC12 Transfer Successful from ${phone} transaction amount SLE${amount} net credit amount SLE${amount} your new balance is SLE100\"}" \
   -X POST https://stable-sl-coordinator.pdj.app/api/sms_received
