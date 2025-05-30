d1345554a1775149f7d5614e2b24900439c02d91
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2025-05-30 02:00:59 -0400

    Improves header

--- packages/react-app/src/components/Header.tsx
+++ packages/react-app/src/components/Header.tsx
@@ -19,12 +19,12 @@ export default function Header() {
 
   return (
         <>
-          {!hideConnectBtn && (
-            <div className="flex h-16 content-center justify-between">
-              <div className="mb-8 text-center">
-                <h1 className="text-3xl font-bold text-gray-900">Stable SL</h1>
-                <p className="text-gray-600 mt-2">Buy USDT in Salone</p>
-              </div>
+          <div className="flex h-16 content-center justify-between">
+            <div className="mb-8 text-center">
+              <h1 className="text-3xl font-bold text-gray-900">Stable SL</h1>
+              <p className="text-gray-600 mt-2">Buy USDT in Salone</p>
+            </div>
+            {!hideConnectBtn && (
               <div className="p-3">
                 <ConnectButton
                   showBalance={{
@@ -33,8 +33,8 @@ export default function Header() {
                   }}
                 />
               </div>
-            </div>
-          )}
+            )}
+          </div>
         </>
   )
 }
